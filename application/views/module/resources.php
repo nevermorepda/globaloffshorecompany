067a84b46e9a83c3141716050c6fad2da9128b78
= <=>
2020-02-15 14:04:56 +0700

    update

--- application/views/module/resources.php
+++ application/views/module/resources.php
@@ -1,14 +1,14 @@
 <?
 	$info = new stdClass();
-	$info->catid = 2;
-	$resources = $this->m_content->items($info,1,6);
+	$info->catid = 6;
+	$resources = $this->m_content->items($info,1,8);
 	$c = count($resources);
 ?>
 <div class=" d-none d-lg-block" id="<?=!empty($tab_detail->module) ? $tab_detail->module : ''?>">
 	<div class="resources wrap-owl">
 		<div class="container">
 			<div style="position: relative;">
-				<h4 class="title text-center">RESOURCES</h4>
+				<h4 class="title text-center">NEWS</h4>
 				<div class="title-line">
 					<div class="line">
 						<img src="<?=IMG_URL?>title-icon.png" alt="">
@@ -23,13 +23,13 @@
 						<div class="mer-item">
 							<div class="bg-item" style="background-image: url(<?=BASE_URL.$resources[0]->thumbnail?>);">
 								<div class="info">
-									<a href="">
+									<a href="<?=site_url("news/{$resources[0]->alias}")?>">
 										<h5 class="title-item"><?=$resources[0]->title?></h5>
 										<p>
 										<?=word_limiter(strip_tags($resources[0]->summary), 35)?>
 										</p>
 									</a>
-									<a href="./resources-detail.html" class="btn-viewmore">View more</a>
+									<a href="<?=site_url("news/{$resources[0]->alias}")?>" class="btn-viewmore">View more</a>
 									<div class="date">
 										<label><?=date('d',strtotime($resources[0]->created_date))?></label><br>
 										<label style="border-top: 1px solid #e6e6e6; font-size: 13px;"><?=date('M',strtotime($resources[0]->created_date))?></label>
@@ -40,18 +40,18 @@
 					</div>
 					<? for ($i=1; $i < $c; $i++) { ?>
 					<div class="item">
-						<a href="">
+						<a href="<?=site_url("news/{$resources[$i]->alias}")?>">
 							<div class="re-item">
 								<div class="bg-re-item" style="background-image: url(<?=BASE_URL.$resources[$i]->thumbnail?>);">
 									<div class="date">
-										<label><?=date('d',strtotime($resources[1]->created_date))?></label><br>
-										<label style="border-top: 1px solid #e6e6e6; font-size: 13px;"><?=date('M',strtotime($resources[1]->created_date))?></label>
+										<label><?=date('d',strtotime($resources[$i]->created_date))?></label><br>
+										<label style="border-top: 1px solid #e6e6e6; font-size: 13px;"><?=date('M',strtotime($resources[$i]->created_date))?></label>
 									</div>
 								</div>
 							</div>
 							<h5 class="title-re-item"><?=$resources[$i]->title?></h5>
 						</a>
-						<a href="./resources-detail.html" class="btn btn-viewmore">view more</a>
+						<a href="<?=site_url("news/{$resources[$i]->alias}")?>" class="btn btn-viewmore">view more</a>
 					</div>
 					<? } ?>
 				</div>
